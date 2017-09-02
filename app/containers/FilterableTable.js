aa9ee8b7e703c3e10929a8ac878913e3acb9c0c9
furixturi <alabebop@gmail.com>
2017-09-02 21:01:16 +0900

    update FilterableTable container

--- app/containers/FilterableTable.js
+++ app/containers/FilterableTable.js
@@ -1,13 +1,15 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-type';
+import React from 'react';
 import { connect } from 'react-redux';
 import { filterTable } from '../actions';
 import ProductTable from '../components/ProductTable';
+import { filterableTable } from '../styles/filterableTable.scss';
 
 const FilterableTable = ({ filter, onFilter }) => {
     let input;
 
     return (
-        <div className="filterable-table">
+        <div className={filterableTable}>
             <input
                 value={filter}
                 ref={node => {input = node;}}
--- app/index.js
+++ app/index.js
@@ -6,7 +6,6 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { configureStore, history } from './store/configureStore';
 import Root from './containers/Root';
-// import './styles/main.scss';
 
 const store = configureStore();
 // const history = syncHistoryWithStore(browserHistory, store);
