18aa96ba82da88ae395e119e22a29ff75d062739
furixturi <alabebop@gmail.com>
2017-09-02 21:19:43 +0900

    clran up index.js

--- app/index.js
+++ app/index.js
@@ -1,14 +1,10 @@
-// import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-// import { browserHistory } from 'react-router';
-// import { syncHistoryWithStore } from 'react-router-redux';
 import { AppContainer } from 'react-hot-loader';
 import { configureStore, history } from './store/configureStore';
 import Root from './containers/Root';
 
 const store = configureStore();
-// const history = syncHistoryWithStore(browserHistory, store);
 
 render(
     <AppContainer>
