0d9c1df051c27c9a107d7444800fdc9ecb6e1b8a
furixturi <alabebop@gmail.com>
2017-09-02 20:31:28 +0900

    update index.js

--- app/index.js
+++ app/index.js
@@ -1,15 +1,15 @@
-import 'babel-polyfill';
+// import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+// import { browserHistory } from 'react-router';
+// import { syncHistoryWithStore } from 'react-router-redux';
 import { AppContainer } from 'react-hot-loader';
-import configureStore from './store/configureStore';
+import { configureStore, history } from './store/configureStore';
 import Root from './containers/Root';
-import './styles/main.scss';
+// import './styles/main.scss';
 
 const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
+// const history = syncHistoryWithStore(browserHistory, store);
 
 render(
     <AppContainer>
