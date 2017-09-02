f94532200e8d49e2a88e0b6d9c2d5bdc977cfcf6
furixturi <alabebop@gmail.com>
2017-09-02 16:11:13 +0900

    add content of app entrance index.js

---
+++ app/index.js
@@ -0,0 +1,32 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react';
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { AppContainer } from 'react-hot-loader';
+import configureStore from './store/configureStore';
+import Root from './containers/Root';
+import './styles/main.scss';
+
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+render(
+    <AppContainer>
+        <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById('root')
+);
+
+if(module.hot) {
+    module.hot.accept('./containers/Root', () => {
+        const Root = require('./containers/Root').default;
+        render(
+            <AppContainer>
+                <Root store={store} history={history} />
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
+}
+
